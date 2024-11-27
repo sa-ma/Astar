3fc59117c2fbcfecf8f3d9ddcd59903b2da3984c
Nicholas Deli <92155926+NFDELI@users.noreply.github.com>
2024-11-27 19:40:08 +0100

    Added UCS for tree traversal

--- algorithms/ucs.py
+++ algorithms/ucs.py
@@ -27,7 +27,7 @@ def reconstruct_path(node):
     path.reverse()
     return path
 
-def ucs_pathfind(start_node, goal_node, grid):
+def ucs_pathfind_graph_traversal(start_node, goal_node, grid):
     open_set = []
     closed_set = {}
     start_node.cost = 0 
@@ -50,13 +50,33 @@ def ucs_pathfind(start_node, goal_node, grid):
     
     return None
 
+def ucs_pathfind_tree_traversal(start_node, goal_node, grid):
+    open_set = []
+    start_node.cost = 0
+    heapq.heappush(open_set, (0, start_node))
+    
+    while open_set:
+        current_cost, parent_node = heapq.heappop(open_set)
+        
+        if parent_node.state == goal_node.state:
+            return reconstruct_path(parent_node)
+        
+        for neighbour in get_neighbors(grid, parent_node):
+            new_cost = parent_node.cost + neighbour.cost
+            
+            in_open_set = any(neighbour == node[1] for node in open_set)
+            if(not in_open_set or new_cost < neighbour.cost):
+                neighbour.cost = new_cost
+                neighbour.parent = parent_node
+                heapq.heappush(open_set, (neighbour.cost, neighbour))
+
 def main():
     grid = create_grid(9, 9)
     generate_obstacles(grid, [(0, 7), (1, 7), (3, 2), (2, 2), (4, 3), (6, 6), (6, 5)])
     
     start_node = grid[0][0]
     goal_node = grid[5][5]
-    path = ucs_pathfind(start_node, goal_node, grid)
+    path = ucs_pathfind_graph_traversal(start_node, goal_node, grid)
     
     if path:
         print("Path found:", path)

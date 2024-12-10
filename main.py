ee7263c9d170b9d691d9a71689087461f05bd85a
Grunex <grunexx@gmail.com>
2024-12-10 19:08:53 +0000

    dfs update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from algorithms import bfs, ucs
+from algorithms import bfs, dfs, ucs
 
 def main():
-    choice = input("Choose algorithm (bfs/ucs): ").strip().lower()
-    if choice == "bfs":
-        bfs.main()
-    elif choice == "ucs":
-        ucs.main()
-    else:
-        print("Invalid choice.")
+    # for quick testing only: (uncomment rest of the code to choose)
+    bfs.main()
+    dfs.main()  
+    # choice = input("Choose algorithm (bfs/dfs/ucs): ").strip().lower()
+    # if choice == "bfs":
+    #     bfs.main()
+    # elif choice == "dfs":
+    #     dfs.main()   
+    # elif choice == "ucs":
+    #     ucs.main()
+    # else:
+    #     print("Invalid choice.")
 
 if __name__ == "__main__":
     main()

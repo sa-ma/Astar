82a5961aa1d92b5a78dadf3fd6449ced22b763c0
thanktua-spp <egbethankgodjnr@gmail.com>
2025-01-03 15:51:00 +0000

    comment out dfs call in main function for testing purposes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from algorithms import bfs, dfs, ucs
 def main():
     # for quick testing only: (uncomment rest of the code to choose)
     bfs.main()
-    dfs.main()  
+    #dfs.main()  
     # choice = input("Choose algorithm (bfs/dfs/ucs): ").strip().lower()
     # if choice == "bfs":
     #     bfs.main()
